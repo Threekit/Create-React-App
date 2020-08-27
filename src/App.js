819adb8e188b38b8b3e91b4591adcc8692f6dfc7
Phil Bredeson <phil.bredeson@gmail.com>
2020-08-27 16:42:48 -0500

    Added some initial styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import { usePlayer } from "./Threekit";
 
 function App() {
   const playerRef = usePlayer({
-    assetId: "7479a7f6-bae4-4d3a-94c7-5df6f6e04dae",
+    assetId: "406bd3c3-f3b4-4af9-9b5f-64e69cfbdd3f",
     authToken: process.env.REACT_APP_THREEKIT_AUTH_TOKEN,
     showAR: true,
+    showConfigurator: true,
   });
 
   return (
     <div className="App">
-      <div className="player-el" ref={playerRef} />
+      <div className="App-header">
+        <a href="https://www.threekit.com/">
+          <img alt="logo" src="https://i.imgur.com/zsXaU2U.png" width="200px" />
+        </a>
+      </div>
+      <div className="App-main">
+        <div className="player-el" ref={playerRef} />
+      </div>
     </div>
   );
 }
